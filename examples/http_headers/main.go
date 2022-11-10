71b1ad7a2f4a044d9a64411e967f0eea67ad245f
CMeadows <CMeadows@localhost>
2022-11-10 10:15:55 -0600

    code for decoding JWT header and rejecting those with alg none

--- examples/http_headers/main.go
+++ examples/http_headers/main.go
@@ -17,7 +17,11 @@ package main
 import (
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
+	"strings"
+	"encoding/base64"
+	// "github.com/golang-jwt/jwt/v4"
 )
+const clusterName = "web_service"
 
 func main() {
 	proxywasm.SetVMContext(&vmContext{})
@@ -61,17 +65,41 @@ func (ctx *httpHeaders) OnHttpRequestHeaders(numHeaders int, endOfStream bool) t
 
 	for _, h := range hs {
 		proxywasm.LogInfof("request header --> %s: %s", h[0], h[1])
+		if h[0] == "cookie" {
+			// cookie was set with JWT 
+			if h[1] != "" {
+				proxywasm.LogInfof("going to authenticate this request")
+				token := strings.SplitAfterN(h[1], "token=", 2)
+				proxywasm.LogInfof("this is our extracted token: %s", token[1])
+
+				// decode token header to extract alg
+				header_payload := strings.Split(token[1], ".")
+				header, err := base64.StdEncoding.DecodeString(header_payload[0])
+				if err != nil {
+					proxywasm.LogInfof("some err thrown when decoding header")
+				}
+				alg := strings.Split(string(header), "{\"typ\":\"JWT\",\"alg\":\"")
+				
+				if alg[1] == "none" {
+					if _, err := proxywasm.DispatchHttpCall(clusterName, hs, nil, nil,
+						50000, httpCallResponseCallback); err != nil {
+						proxywasm.LogCriticalf("dipatch httpcall failed: %v", err)
+						return types.ActionContinue
+					}
+				}
+			}
+		}
 	}
 	
 	path, err := proxywasm.GetHttpRequestHeader("path")
 	if err != nil { // check if path field has been set yet (i.e. are we the first on the path?)
-		err := proxywasm.ReplaceHttpRequestHeader("path", "IP here")
+		err := proxywasm.ReplaceHttpRequestHeader("path", "IP here") // TODO: extract IP to set
 		if err != nil {
 			proxywasm.LogCritical("failed to set request header: path")
 		}
 		proxywasm.LogInfof("request header --> path: IP here")
-	} else {
-		proxywasm.LogInfof("should not be here: %s", path)
+	} else { // TODO: extract path and append new IP to it
+		proxywasm.LogInfof("should not be here for now: %s", path)
 	}
 
 	return types.ActionContinue
@@ -93,4 +121,17 @@ func (ctx *httpHeaders) OnHttpResponseHeaders(numHeaders int, endOfStream bool) 
 // Override types.DefaultHttpContext.
 func (ctx *httpHeaders) OnHttpStreamDone() {
 	proxywasm.LogInfof("%d finished", ctx.contextID)
+}
+
+func httpCallResponseCallback(numHeaders, bodySize, numTrailers int) {
+	
+	body := "access forbidden: JWT has alg none!!"
+	proxywasm.LogInfo(body)
+	if err := proxywasm.SendHttpResponse(403, [][2]string{
+		{"powered-by", "proxy-wasm-go-sdk!!"},
+	}, []byte(body), -1); err != nil {
+		proxywasm.LogErrorf("failed to send local response: %v", err)
+		proxywasm.ResumeHttpRequest()
+	}
+		
 }
