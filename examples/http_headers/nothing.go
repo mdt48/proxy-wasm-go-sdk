27090b2e5164566ebb8b5c741d6a073aa7bfb344
mdt <mdt_@gwu.edu>
2023-04-04 13:56:35 -0500

    nothing

---
+++ examples/http_headers/nothing.go
@@ -0,0 +1,88 @@
+// Copyright 2020-2021 Tetrate
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
+)
+
+const tickMilliseconds uint32 = 1000
+
+func main() {
+	proxywasm.SetVMContext(&vmContext{})
+}
+
+type vmContext struct {
+	// Embed the default VM context here,
+	// so that we don't need to reimplement all the methods.
+	types.DefaultVMContext
+}
+
+// Override types.DefaultVMContext.
+func (*vmContext) NewPluginContext(contextID uint32) types.PluginContext {
+	return &pluginContext{contextID: contextID}
+}
+
+type pluginContext struct {
+	// Embed the default plugin context here,
+	// so that we don't need to reimplement all the methods.
+	types.DefaultPluginContext
+	contextID uint32
+	callBack  func(numHeaders, bodySize, numTrailers int)
+	cnt       int
+}
+
+// Override types.DefaultPluginContext.
+func (ctx *pluginContext) OnPluginStart(pluginConfigurationSize int) types.OnPluginStartStatus {
+	if err := proxywasm.SetTickPeriodMilliSeconds(tickMilliseconds); err != nil {
+		proxywasm.LogCriticalf("failed to set tick period: %v", err)
+		return types.OnPluginStartStatusFailed
+	}
+	return types.OnPluginStartStatusOK
+}
+
+type httpHeaders struct {
+	// Embed the default http context here,
+	// so that we don't need to reimplement all the methods.
+	types.DefaultHttpContext
+	contextID   uint32
+	headerName  string
+	headerValue string
+	cnt         int
+	callBack    func(numHeaders, bodySize, numTrailers int)
+	plugin      pluginContext
+
+	// cnt       int
+}
+
+func (p *pluginContext) NewHttpContext(contextID uint32) types.HttpContext {
+
+	return &httpHeaders{
+		contextID: contextID,
+		callBack:  p.callBack,
+		plugin:    *p,
+	}
+}
+
+func (ctx *httpHeaders) OnHttpRequestHeaders(_ int, _ bool) types.Action {
+
+	return types.ActionContinue
+}
+
+func (ctx *httpHeaders) OnHttpResponseHeaders(_ int, _ bool) types.Action {
+
+	return types.ActionContinue
+}
